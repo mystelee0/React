a94f210080ee13a887bcfc1b0275d99048121b01
mystelee <lms8756@naver.com>
2023-01-27 17:51:23 +0900

    리액트 라우터 사용

---
+++ src/pages/Detail.js
@@ -0,0 +1,24 @@
+import { useParams } from "react-router-dom";
+
+function Detail(props){
+
+  let {id}=useParams();
+  
+  return (
+        <div className="container">
+  <div className="row">
+    <div className="col-md-6">
+      <img src={`https://codingapple1.github.io/shop/shoes${parseInt(id)+1}.jpg`} alt="" width="100%" />
+    </div>
+    <div className="col-md-6">
+      <h4 className="pt-5">{props.shoes[id].title}</h4>
+      <p>{props.shoes[id].content}</p>
+      <p>{props.shoes[id].price}</p>
+      <button className="btn btn-danger">주문하기</button> 
+    </div>
+  </div>
+</div> 
+    );
+}
+
+export default Detail;
